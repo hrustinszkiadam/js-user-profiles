f2f383381acdc059ffeedb626ca9f912f475f438
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-12-06 09:12:05 +0100

    feat: added compatibility for Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import UsersProvider from './contexts/UsersContext.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<UsersProvider>
-			<App />
-		</UsersProvider>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='*'
+					element={
+						<UsersProvider>
+							<App />
+						</UsersProvider>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
 	</StrictMode>,
 );
